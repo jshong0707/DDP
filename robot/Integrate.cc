8a7ae11eeb30073f7d768e07e8acedf2f4e9b5d3
jshong0707 <hn05198@gmail.com>
2025-07-15 21:52:42 +0900

    change integrate as pimpl

--- robot/Integrate.cc
+++ robot/Integrate.cc
@@ -1,137 +1,155 @@
 #include "Integrate.hpp"
+#include "robot_parameter.hpp"
+#include "Trajectory.hpp"
+#include "Body.hpp"
+#include "MPC.hpp"
+#include "Controller.hpp"
+#include "FSM.hpp"
+#include <array>
 
+struct Integrate::Impl {
+  robot_parameter &pino;
+  Trajectory      &Traj;
+  Body            &B;
+  MPC             &M;
+  Controller      &C;
+  FSM             &FSM_;
 
-Integrate::Integrate(robot_parameter &pino, Trajectory &Traj, Body &B, MPC &M, Controller &C, FSM &FSM_)
-:pino(pino), Traj(Traj), B(B), M(M), C(C), FSM_(FSM_)
-{
-    leg_pos_ref = VectorXd::Zero(12);
-    leg_pos = VectorXd::Zero(12);
-    
+  double t = 0;
+  double optimization_t = 0;
+  double mpc_dt = 0;
+
+  Eigen::VectorXd q, qd, qdd;
+  Eigen::Vector3d FB_input[4];
+  Eigen::Vector3d F_Joint_input[2];
+  Eigen::Vector3d B_Joint_input[2];
+  Eigen::VectorXd opt_u;
+  Eigen::Vector3d opt_GRF[4];
+  bool Contact[4];
+  bool Contact_traj[4];
+
+  Eigen::VectorXd leg_pos;
+  Eigen::VectorXd leg_pos_ref;
+  Eigen::Vector3d pos_err[4], pos_err_old[4];
+  std::vector<bool> is_contact;
+
+  Impl(robot_parameter &p,
+       Trajectory      &t_,
+       Body            &b,
+       MPC             &m,
+       Controller      &c,
+       FSM             &f)
+    : pino(p),
+      Traj(t_),
+      B(b),
+      M(m),
+      C(c),
+      FSM_(f),
+      q(Eigen::VectorXd::Zero(19)),
+      qd(Eigen::VectorXd::Zero(18)),
+      qdd(Eigen::VectorXd::Zero(18)),
+      opt_u(Eigen::VectorXd::Zero(12)),
+      leg_pos(Eigen::VectorXd::Zero(12)),
+      leg_pos_ref(Eigen::VectorXd::Zero(12)),
+      is_contact(4, false)
+  {
     mpc_dt = M.get_dt();
-    opt_u.resize(12); opt_u.setZero();
-}
-
-Integrate::~Integrate()
-{
-}
-
-void Integrate::sensor_measure(const mjModel* m, mjData* d)
-{
-    t = d->time;
-
-    for(int i = 0; i < 19; i++) q[i] = d->qpos[i];
-    for(int i = 0; i < 18; i++) qd[i] = d->qvel[i];
-    for(int i = 0; i < 18; i++) qdd[i] = d->qacc[i];
-    
-    pino.robot_param(q, qd, qdd);
-
-    B.sensor_measure(m, d);
-    M.foot_vector(m, d);
-
-
-    // Matrix3d M_mujoco = Matrix3d::Zero();
-    // // joint index 추출
-    // int idx[3];
-    // for (int i = 0; i < 3; ++i)
-    // {
-    //     idx[i] = 3 * i;
-    //     // cout << "idx: " << idx[i] << endl;  FL FR 각각 123 456
-    // }
-    // mj_forward(m, d);
-    // MatrixXd M_full(m->nv, m->nv); //m->nv는 18
-    // mj_fullM(m, M_full.data(), d->qM);
-    // M_mujoco = M_full.block<3, 3>(idx[0]+6, idx[0]+6);
-    // mj_rnePostConstraint(m, d);
-    // Vector3d bias;
-    // for (int i = 0; i < 3; ++i)
-    //     bias(i) = d->qfrc_bias[idx[i]+6];
-    // mjData* d_zero = mj_makeData(m);
-    // for (int i = 0; i < m->nq; ++i)
-    // {   d_zero->qpos[i] = d->qpos[i];
-    // }
-    // for (int i = 0; i < m->nv; ++i)
-    //     d_zero->qvel[i] = 0;
-    // mj_forward(m, d_zero);
-    // mj_rnePostConstraint(m, d_zero);
-    // // for (int i = 0; i < 3; ++i)
-    // //     G_mujoco(i) = d_zero->qfrc_bias[idx[i]+5];
-    // // C_mujoco = bias - G_mujoco;
-    // // mj_deleteData(d_zero);
-
-    // cout << "M matrix\n" << M_mujoco << endl;
-
-}
-
-void Integrate::Leg_controller()
-{
-    /* State Machine */
-    is_contact = FSM_.contactschedule();
-
-    /* FeedBack Controller */
-    for(int leg = 0; leg < 4; leg++)
-    {
-        if(is_contact[leg] == false)
-            FB_input[leg] = C.FB_controller(pos_err[leg], pos_err_old[leg], leg);
-        else
-            FB_input[leg] = Vector3d::Zero();
+    // 초기화: FB_input, opt_GRF, pos_err, Contact 등
+    for(int i=0;i<4;i++){
+      FB_input[i].setZero();
+      opt_GRF[i].setZero();
+      pos_err[i].setZero();
+      pos_err_old[i].setZero();
+      Contact[i] = Contact_traj[i] = true;
     }
+  }
+};
 
-    /* MPC */
-    if(t >= optimization_t)
-    {
-        M.Dynamics();
-        M.SolveQP();
+Integrate::Integrate(robot_parameter &pino,
+                     Trajectory      &Traj,
+                     Body            &B,
+                     MPC             &M,
+                     Controller      &C,
+                     FSM             &FSM_)
+  : pimpl_(std::make_unique<Impl>(pino, Traj, B, M, C, FSM_))
+{}
 
-        opt_u = M.get_opt_u();
-        
-        if(t < 0.00000001)
-            opt_u = VectorXd::Zero(12);
+Integrate::~Integrate() = default;
 
-        for(int i = 0; i < 4; i ++)
-        {
-            opt_u[3*i] = - opt_u[3*i];
-            opt_u[3*i + 1] = - opt_u[3*i + 1];
-            opt_GRF[i] = B.get_R().transpose() * opt_u.segment(3*i,3);
-        }
+void Integrate::sensor_measure(const mjModel* m, mjData* d) {
+  auto &i = *pimpl_;
+  i.t = d->time;
+  for(int k=0;k<19;k++) i.q[k] = d->qpos[k];
+  for(int k=0;k<18;k++) i.qd[k] = d->qvel[k];
+  for(int k=0;k<18;k++) i.qdd[k] = d->qacc[k];
 
-        // cout << "///////////////////////////////////////////////////////////\n" << endl;
-        // cout << is_contact << endl;
-        // cout << opt_GRF[0][0] << "   " << opt_GRF[1][0] << "   " << opt_GRF[2][0] << "   " << opt_GRF[3][0] << endl; 
-        // cout << opt_GRF[0][1] << "   " << opt_GRF[1][1] << "   " << opt_GRF[2][1] << "   " << opt_GRF[3][1] << endl; 
-        // cout << opt_GRF[0][2] << "   " << opt_GRF[1][2] << "   " << opt_GRF[2][2] << "   " << opt_GRF[3][2] << endl; 
-        
-        optimization_t += mpc_dt;
+  i.pino.robot_param(i.q, i.qd, i.qdd);
+  i.B.sensor_measure(m, d);
+  i.M.foot_vector(m, d);
+}
+
+void Integrate::get_error(double /*unused*/) {
+  auto &i = *pimpl_;
+  i.leg_pos_ref = i.Traj.Traj(i.t);
+  for(int k=0;k<4;k++) i.pos_err_old[k] = i.pos_err[k];
+  for(int k=0;k<4;k++){
+    i.pos_err[k] = i.leg_pos_ref.segment<3>(3*k)
+                 - i.pino.get_leg_pos(k);
+  }
+}
+
+void Integrate::Leg_controller() {
+  auto &i = *pimpl_;
+
+  i.is_contact = i.FSM_.contactschedule();
+  for(int k=0;k<4;k++){
+    if(!i.is_contact[k])
+      i.FB_input[k] = i.C.FB_controller(i.pos_err[k], i.pos_err_old[k], k);
+    else
+      i.FB_input[k] = Eigen::Vector3d::Zero();
+  }
+
+  if(i.t >= i.optimization_t) {
+    i.M.Dynamics();
+    i.M.SolveQP();
+    i.opt_u = i.M.get_opt_u();
+    if(i.t < 1e-8) i.opt_u.setZero();
+    for(int k=0;k<4;k++){
+        i.opt_u[3*k] = -i.opt_u[3*k];
+        i.opt_u[3*k + 1] = -i.opt_u[3*k + 1];
+
+      i.opt_GRF[k] = i.B.get_R().transpose()
+                     * (i.opt_u.segment<3>(3*k));
     }
+    i.optimization_t += i.mpc_dt;
+  }
 
-    /* Joint Input */
-
-        F_Joint_input[0] = pino.get_Jacb(0).transpose() * (opt_GRF[0] + FB_input[0]);
-        F_Joint_input[1] = pino.get_Jacb(1).transpose() * (opt_GRF[1] + FB_input[1]);
-        B_Joint_input[0] = pino.get_Jacb(2).transpose() * (opt_GRF[2] + FB_input[2]);
-        B_Joint_input[1] = pino.get_Jacb(3).transpose() * (opt_GRF[3] + FB_input[3]);
-
-
-}
-
-
-void Integrate::get_error(double t)
-{
-    leg_pos_ref = Traj.Traj(t);
-    
-    // cout << leg_pos_ref << endl;
-
-    for(int i = 0; i < 4; i ++)
-        pos_err_old[i] = pos_err[i];
-
-    pos_err[0] = leg_pos_ref.segment(0,3) - pino.get_leg_pos(0);
-    pos_err[1] = leg_pos_ref.segment(3,3) - pino.get_leg_pos(1);
-    pos_err[2] = leg_pos_ref.segment(6,3) - pino.get_leg_pos(2);
-    pos_err[3] = leg_pos_ref.segment(9,3) - pino.get_leg_pos(3);
+  i.F_Joint_input[0] = i.pino.get_Jacb(0).transpose()
+                      * (i.opt_GRF[0] + i.FB_input[0]);
+  i.F_Joint_input[1] = i.pino.get_Jacb(1).transpose()
+                      * (i.opt_GRF[1] + i.FB_input[1]);
+  i.B_Joint_input[0] = i.pino.get_Jacb(2).transpose()
+                      * (i.opt_GRF[2] + i.FB_input[2]);
+  i.B_Joint_input[1] = i.pino.get_Jacb(3).transpose()
+                      * (i.opt_GRF[3] + i.FB_input[3]);
 
 }
 
-void Integrate::Data_log()
-{
-    leg_pos << pino.get_leg_pos(0), pino.get_leg_pos(1), pino.get_leg_pos(2), pino.get_leg_pos(3);
+void Integrate::Data_log() {
+  auto &i = *pimpl_;
+  for(int k=0;k<4;k++){
+    i.leg_pos.segment<3>(3*k)
+      = i.pino.get_leg_pos(k);
+  }
+}
 
-}   
+Eigen::Vector3d Integrate::get_FL_J_input() { return pimpl_->F_Joint_input[0]; }
+Eigen::Vector3d Integrate::get_FR_J_input() { return pimpl_->F_Joint_input[1]; }
+Eigen::Vector3d Integrate::get_RL_J_input() { return pimpl_->B_Joint_input[0]; }
+Eigen::Vector3d Integrate::get_RR_J_input() { return pimpl_->B_Joint_input[1]; }
+
+Eigen::VectorXd Integrate::get_leg_pos_ref() { return pimpl_->leg_pos_ref; }
+Eigen::VectorXd Integrate::get_leg_pos()     { return pimpl_->leg_pos; }
+Eigen::VectorXd Integrate::get_x0()          { return pimpl_->B.get_x0(); }
+Eigen::VectorXd Integrate::get_x_ref()       { return pimpl_->B.get_x_ref(pimpl_->t); }
+Eigen::VectorXd Integrate::get_opt_GRF(int leg) { return pimpl_->opt_GRF[leg]; }
--- robot/Integrate.hpp
+++ robot/Integrate.hpp
@@ -1,99 +1,46 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+#include <mujoco/mjmodel.h>
+#include <mujoco/mjdata.h>
 #include "globals.hpp"
-#include "Actuator.hpp"
-#include "F_Kinematics.hpp"
-#include "B_Kinematics.hpp"
-#include "Controller.hpp"
-#include "Body.hpp"
-#include "MPC.hpp"
-#include "FSM.hpp"
-#include "robot_parameter.hpp"
 
-class Controller;
-class F_Kinematics;
-class B_Kinematics;
+class robot_parameter;
 class Trajectory;
 class Body;
 class MPC;
-class robot_parameter;
+class Controller;
+class FSM;
 
 class Integrate
 {
-private:
-    /* Class declaration */
-        robot_parameter &pino;
-        Trajectory &Traj;
-        Body &B;
-        Controller &C;
-        MPC &M;
-        FSM &FSM_;
-        // QP qp;
-
-        double t;
-
-    /* Joint State */
-        VectorXd q = VectorXd::Zero(19);
-        VectorXd qd = VectorXd::Zero(18);
-        VectorXd qdd = VectorXd::Zero(18);
-        
-    /* FeedBack input*/
-        Vector3d FB_input[4];
-
-    /* Joint input*/
-        Vector3d F_Joint_input[2];
-        Vector3d B_Joint_input[2];
-
-    /* Optimized Value */
-        double optimization_t = 0;
-        VectorXd opt_u;  
-        Vector3d opt_GRF[4];
-        Vector3d opt_tau[4];
-        bool Contact[4] = {1, 1, 1, 1};
-        bool Contact_traj[4];
-    
-    /* MPC */
-        double mpc_dt;
-        
-    /* Leg State */
-        VectorXd leg_pos;
-        
-        /* Trajectory */
-        VectorXd leg_pos_ref;
-
-        
-        Vector3d pos_err[4];
-        Vector3d pos_err_old[4];
-
-    /* Contact Schedule */
-        vector<bool> is_contact = {false, false, false, false};
-
 public:
-    Integrate(robot_parameter &pino, Trajectory &Traj, Body &B, MPC &M, Controller &C, FSM &FSM_);
-    ~Integrate();
-    void sensor_measure(const mjModel* m, mjData* d);
-    void get_error(double t);
-    void Leg_controller();
-    void Data_log();
+  Integrate(robot_parameter &pino,
+            Trajectory      &Traj,
+            Body            &B,
+            MPC             &M,
+            Controller      &C,
+            FSM             &FSM_);
+  ~Integrate();
 
+  void sensor_measure(const mjModel* m, mjData* d);
+  void get_error(double t);
+  void Leg_controller();
+  void Data_log();
 
-    Vector3d get_FL_J_input() {return F_Joint_input[0];}
-    Vector3d get_FR_J_input() {return F_Joint_input[1];}
-    Vector3d get_RL_J_input() {return B_Joint_input[0];}
-    Vector3d get_RR_J_input() {return B_Joint_input[1];}
-    
-    
-    
-    /* Data Logging */
-    VectorXd get_leg_pos_ref() {return leg_pos_ref;}
-    VectorXd get_leg_pos() {return leg_pos;}
+  Eigen::Vector3d get_FL_J_input();
+  Eigen::Vector3d get_FR_J_input();
+  Eigen::Vector3d get_RL_J_input();
+  Eigen::Vector3d get_RR_J_input();
 
-    VectorXd get_x0() {return B.get_x0();}
-    VectorXd get_x_ref() {return B.get_x_ref(t);}
-    
-    VectorXd get_opt_GRF(int leg) {return opt_GRF[leg];}
-    // Vector3d get_leg_pos_ref(int Leg_num) {return leg_pos_ref[Leg_num];}
-    // Vector3d get_leg_vel_ref(int Leg_num) {return leg_vel_ref[Leg_num];}
-    // Vector3d get_leg_pos(int Leg_num) {return leg_pos[Leg_num];}
-    // Vector3d get_leg_vel(int Leg_num) {return leg_vel[Leg_num];}
+  Eigen::VectorXd get_leg_pos_ref();
+  Eigen::VectorXd get_leg_pos();
+  Eigen::VectorXd get_x0();
+  Eigen::VectorXd get_x_ref();
+  Eigen::VectorXd get_opt_GRF(int leg);
+
+private:
+  struct Impl;
+  std::unique_ptr<Impl> pimpl_;
 };
