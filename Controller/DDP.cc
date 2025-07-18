6dd93e5f2ab1d8e142325602a8dd590484c755d1
jshong0707 <hn05198@gmail.com>
2025-07-18 10:19:17 +0900

    Modify MJCF for matching with URDF

--- Controller/DDP.cc
+++ Controller/DDP.cc
@@ -1,9 +1,36 @@
 #include "DDP.hpp"
+#include "robot_parameter.hpp"
+#include <array>
 
-DDP::DDP(/* args */)
-{
-}
+struct DDP::Impl {
 
-DDP::~DDP()
-{
-}
+    std::vector<std::shared_ptr<crocoddyl::CostModelAbstract>> Cost;
+    
+    const double dt = 0.001;
+    Eigen::VectorXd x0 = Eigen::VectorXd::Zero(36);
+    Eigen::VectorXd state = Eigen::VectorXd::Zero(36);
+    MatrixXd A = MatrixXd::Zero(36,36);
+
+    // std::shared_ptr<crocoddyl::DifferentialActionModelContactFwdDynamics>
+    // auto runningModel = std::make_shared<crocoddyl::ActionModelLQR>(state, A, B, Q, R, dt);
+
+    // std::vector<std::shared_ptr<crocoddyl::ActionModelAbstract>> runningModels(T, runningModels);
+
+
+    // auto state = std::make_shared<crocoddyl::StateVector>(nx);
+    // std::make_shared<crocoddyl::SolverFDDP>
+    Impl()
+    {        
+        // Cost.push_back(std::make_shared<crocoddyl::CostModelSum>(state,nu));   
+    } // = default;
+
+    // auto state = std::make_shared<crocoddyl::>
+      
+};
+
+
+
+DDP::DDP(): pimpl_(std::make_unique<Impl>())
+{}
+
+DDP::~DDP() = default;
--- Controller/DDP.hpp
+++ Controller/DDP.hpp
@@ -4,14 +4,25 @@
 #include <memory>
 #include <Eigen/Dense>
 #include <vector>
-#include <crocoddyl/core/utils/callbacks.hpp>
+#include <crocoddyl/core/solvers/ddp.hpp>
+#include <crocoddyl/core/actions/lqr.hpp>
+#include <crocoddyl/core/integrator/euler.hpp>
+#include <crocoddyl/core/states/euclidean.hpp>
+#include <crocoddyl/core/utils/timer.hpp>
+#include <crocoddyl/core/solvers/fddp.hpp>
+#include <crocoddyl/multibody/states/multibody.hpp>
+#include <crocoddyl/multibody/actions/free-fwddyn.hpp>
+
+
 
 class DDP
 {
-private:
-    
 public:
-    DDP(/* args */);
+    DDP();
     ~DDP();
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> pimpl_;
 };
 
